cfe501fca6cd389cf17cdcbe73216ef19424f805
Akthab <mhmdakthab6@gmail.com>
2024-05-15 16:52:02 +0530

    feat(): worked with plugins

--- cypress.config.js
+++ cypress.config.js
@@ -12,3 +12,5 @@ module.exports = defineConfig({
 		// defaultCommandTimeout: 6000
 	},
 });
+
+require('@applitools/eyes-cypress')(module);
---
+++ cypress/e2e/plugins.cy.js
@@ -0,0 +1,17 @@
+beforeEach(() => {
+	cy.eyesOpen({
+		appName: 'Trello app',
+	});
+});
+
+it('plugins', () => {
+	cy.visit('/');
+
+	cy.get('[data-cy=board-item]').should('be.visible');
+
+	cy.eyesCheckWindow();
+});
+
+afterEach(() => {
+	cy.eyesClose();
+});
